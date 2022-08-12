3a2e0685939433a9c77ed80f3e7f299e782cbe2f
Praise Balogun <pbalogun@axxess.com>
2022-08-12 01:40:53 +0100

    added cors and token services

---
+++ PasswordManager.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace PasswordManager.Api.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection ConfigureCors(this IServiceCollection service)
+        {
+            service.AddCors(option =>
+            {
+                option.AddPolicy("AllowOrigins", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+            return service;
+        }
+    }
+}
---
+++ PasswordManager.Core/Application/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using PasswordManager.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Core.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
