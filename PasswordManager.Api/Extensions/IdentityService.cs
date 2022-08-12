390f94f0e050dfb687973515fc9bb3243f899747
Praise Balogun <pbalogun@axxess.com>
2022-08-12 15:55:26 +0100

    Added Identity service

---
+++ PasswordManager.Api/Extensions/IdentityService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using PasswordManager.Core.Domain.Entities;
+using PasswordManager.Infrastructure.Data.IdentityContext;
+using System.Text;
+
+namespace PasswordManager.Api.Extensions
+{
+    public static class IdentityService
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection service, IConfiguration config)
+        {
+            var builder = service.AddIdentityCore<AppUser>();
+            builder = new IdentityBuilder(builder.UserType, builder.Services);
+            builder.AddEntityFrameworkStores<UserDbContext>();
+            builder.AddSignInManager<SignInManager<AppUser>>();
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option =>
+                {
+                    option.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                        ValidIssuer = config["Token:Issuer"],
+                        ValidateIssuer = true
+                    };
+                });
+            return service;
+        }
+    }
+}
