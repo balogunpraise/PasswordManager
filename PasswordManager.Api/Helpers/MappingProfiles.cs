1a7bdfdeb22e41ba2a33691ea2df8f218905534e
Praise Balogun <pbalogun@axxess.com>
2022-08-20 12:52:01 +0100

    updated the mapping profile

--- PasswordManager.Api/Helpers/MappingProfiles.cs
+++ PasswordManager.Api/Helpers/MappingProfiles.cs
@@ -1,13 +1,16 @@
 ﻿using AutoMapper;
 using PasswordManager.Api.Dtos;
+using PasswordManager.Api.Services;
 using PasswordManager.Core.Domain.Entities;
 
 namespace PasswordManager.Api.Helpers
 {
     public class MappingProfiles : Profile
     {
+        private readonly EncryptionService _encyptionService;
         public MappingProfiles()
         {
+            _encyptionService = new EncryptionService();
             CreateMap<LoginCredential, LoginCredentialsDto>();
         }
     }
